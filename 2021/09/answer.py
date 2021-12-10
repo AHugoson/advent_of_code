fb6aa4a4a00c469bdc0e5ac32d006118f881e812
AHugoson <aron.hugoson@simumatik.com>
2021-12-10 15:40:39 +0100

    Even more incomplete?

--- 2021/09/answer.py
+++ 2021/09/answer.py
@@ -18,24 +18,24 @@ def is_low(x, y, entries):
         low = False
     return low
 
-def get_basin(x, y, entries, basin_size=0):
+def get_basin(x, y, entries, basin_size=0, already_counted=[]):
     n = entries[y][x]
-    print(x, y, n)
-    if x > 0 and n == entries[y][x-1] - 1 < 10:
-        # print(n, entries[y][x-1], basin_size)
-        return get_basin(x-1, y, entries, basin_size+1)
-    if x < len(entries[y])-1 and n == entries[y][x+1] - 1 < 10:
-        # print(n, entries[y][x+1], basin_size)
-        return get_basin(x+1, y, entries, basin_size+1)
-    if y > 0 and n == entries[y-1][x] - 1 < 10:
-        # print(n, entries[y-1][x], basin_size)
-        return get_basin(x, y-1, entries, basin_size+1)
-    if y < len(entries)-1 and n == entries[y+1][x] - 1 < 10:
-        # print(n, entries[y+1][x], basin_size)
-        return get_basin(x, y+1, entries, basin_size+1)
-    else:
-        return basin_size
-
+    global basins
+    basins.append(1)
+    tmp_basin = 0
+    if x > 0 and n == entries[y][x-1] - 1 < 8 and [x-1, y] not in already_counted:
+        already_counted.append([x-1, y])
+        tmp_basin += get_basin(x-1, y, entries, basin_size+1)
+    if x < len(entries[y])-1 and n == entries[y][x+1] - 1 < 8 and [x+1, y] not in already_counted:
+        already_counted.append([x+1, y])
+        tmp_basin += get_basin(x+1, y, entries, basin_size+1)
+    if y > 0 and n == entries[y-1][x] - 1 < 8 and [x, y-1] not in already_counted:
+        already_counted.append([x, y-1])
+        tmp_basin += get_basin(x, y-1, entries, basin_size+1)
+    if y < len(entries)-1 and n == entries[y+1][x] - 1 < 8 and [x, y+1] not in already_counted:
+        already_counted.append([x, y+1])
+        tmp_basin += get_basin(x, y+1, entries, basin_size+1)
+    return len(already_counted)
 risk_points = []
 for y in range(len(entries)):
     for x in range(len(entries[0])):
@@ -44,14 +44,19 @@ for y in range(len(entries)):
 
 risk_level = 0
 basins = []
+basin_sizes = []
 for x, y in risk_points:
     risk_level += entries[y][x] + 1
-    print('-------------------')
-    basins.append(get_basin(x, y, entries))
+    get_basin(x, y, entries, 0, [])
+    basin_sizes.append(len(basins))
+    basins = []
 
-print(basins)
+print(sorted(basin_sizes))
 
-basin_3_product = 0
+basin_3_product = 1
+
+for t in sorted(basin_sizes[-3:]):
+    basin_3_product = basin_3_product * t
 
 print('Answer 1:', risk_level)
 print('Answer 2:', basin_3_product)
